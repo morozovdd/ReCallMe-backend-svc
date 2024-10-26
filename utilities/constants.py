df3a1d6fd367a31a9ad4eb67cf87555559921f75
raakhal-rapolu <pandu3011@gmail.com>
2024-10-26 10:53:18 -0400

    added anthropic key

--- utilities/constants.py
+++ utilities/constants.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
 
-gemini_url = os.getenv('GEMINI_URL')
-anthropic_key = os.getenv('ANTHROPIC_API_KEY')
+load_dotenv()
+
+gemini_url = os.environ.get('GEMINI_URL')
+anthropic_key = os.environ.get('ANTHROPIC_API_KEY')
