c2bdb1136975379666e80550b455bf11072de282
dhruv1707 <dmehrottra007@gmail.com>
2024-10-25 20:17:50 -0400

    backend boilerplate

---
+++ api/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
